e45fd9113b213f3b90fcb3cae1a75673ae99f9df
Semantimancer <ben@bkugler.com>
2017-05-20 10:56:12 -0400

    Added tool to help with multi-floored maps

---
+++ togglefloor.js
@@ -0,0 +1,19 @@
+on("change:graphic:left", function(obj,prev){
+    var page = findObjs({ _id: obj.get("_pageid"), type: "page" })[0];
+    var pageName = page.get("name");
+    var objName = obj.get("name");
+    if(objName==pageName){
+        obj.set("left",prev["left"]);
+        obj.set("top",prev["top"]);
+        
+        var ids = obj.get("gmnotes").split("%3Cbr%3E");
+        var objs = filterObjs(function(o){ return ids.indexOf(o.get("id")) != -1; });
+        objs.forEach(function(o){
+            if(o.get("layer")=="gmlayer"){
+                o.set("layer","map");
+            } else {
+                o.set("layer","gmlayer");
+            }
+        });
+    }
+});
